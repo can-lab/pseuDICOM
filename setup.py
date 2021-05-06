23767590f3ceccb97b3ac482318ba0f2d85c4dd7
Florian Krause <siebenhundertzehn@gmail.com>
2021-05-06 13:33:25 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def get_version():
+    """Get version and version_info from finish_the_job/__meta__.py file."""
+
+    import os
+    module_path = os.path.join(os.path.dirname('__file__'), 'pseudicom',
+                               '__meta__.py')
+
+    import importlib.util
+    spec = importlib.util.spec_from_file_location('__meta__', module_path)
+    meta = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(meta)
+    return meta.__version__
+
+__version__ = get_version()
+
+setup(
+    name = 'Finish the job',
+    version = __version__,
+    packages = ['pseudicom'],
+    install_requires = ['nipype',
+                        'pseudicom',
+                        'quickshear'])
